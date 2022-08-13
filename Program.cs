aa9016700905d8f03f56830b9855a353d85b8a88
Denis Eliseev <medved-ed@mail.ru>
2022-08-14 00:33:36 +0800

    Examples 4143 ok

--- Program.cs
+++ Program.cs
@@ -21,7 +21,23 @@ void Zadacha43 ()
     //Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
     //заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем. 
     // пример: b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+    Console.WriteLine ("Для вычисления координат (x, y) пересечения двух прямых воспользуеся формулой");
+    Console.WriteLine ("y = k(i)*x + b(i), где");
+    Console.WriteLine ("k(i)");
+    Console.WriteLine ("b(i)");
+    Console.Write ("Введите значение k1 первой прямой");
+    double k1 = Convert.ToInt32 (Console.ReadLine ());
+    Console.Write ("Введите значение b1 первой прямой");
+    double b1 = Convert.ToInt32 (Console.ReadLine ());
+    Console.Write ("Введите значение k2 первой прямой");
+    double k2 = Convert.ToInt32 (Console.ReadLine ());
+    Console.Write ("Введите значение b2 первой прямой");
+    double b2 = Convert.ToInt32 (Console.ReadLine ());
+    double x;
+    double y;
+    if (k1 == k2) Console.WriteLine ("Прямые - параллельны");
+    else Console.WriteLine ($"Координаты пересечения двух прямых - ({x = (b2 - b1)/(k1 - k2)}, {y = k1 * x + b1})");
 }
 
-Zadacha41 ();
-// Zadacha43 ();
+//Zadacha41 ();
+Zadacha43 ();
